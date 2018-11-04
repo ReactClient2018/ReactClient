5de0ccc5bfcf4553b795639400778ff68faededa
Shree Krishna <shreekrishna905@gmail.com>
2018-11-04 16:32:17 +0545

    updated by utsav

--- src/app/views/forms/CreateNewTenant.jsx
+++ src/app/views/forms/CreateNewTenant.jsx
@@ -1,197 +1,130 @@
-import React, {Component} from 'react';
-import {UserActionCreators} from "actions";
-import PropTypes from 'prop-types';
-import {connect} from "react-redux";
-import Nav from "../../components/Screening/MainAdminNavBar.jsx"
-class CreateNewTenant extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fields: {
-                username: '',
-                password: '',
-                tenantId: '',
-                url: "",
-                driverClassName: "org.postgresql.Driver"
-            },
-            fieldErrors: {}
-        }
 
-    }
-    handleChange = (name, value, error) => {
-        console.log("i am writing");
-        const fields = {
-            ...this.state.fields
-        }; //creating copy of object
-        //or fields={...this.state.fields};
-        const fieldErrors = this.state.fieldErrors;
-        fields[name] = value;
-        fieldErrors[name] = error;
-        this.setState({fields, fieldErrors})
-    };
-    validate = () => {
-        const person = this.state.fields;
-        const fieldErrors = this.state.fieldErrors;
-        const errMessage = Object
-            .keys(fieldErrors)
-            .filter((k) => fieldErrors[k]);
-        if (!person.username) 
-            return true;
-        if (!person.password) 
-            return true;
-        if (!person.tenantId) 
-            return true;
-        if (!person.url) 
-            return true;
-        if (!person.driverClassName) 
-            return true;
-        if (errMessage.length) 
-            return true;
-        return false;
-    }
-    // isEmail = (email) => {     var emailPat =
-    // /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\
-    // 
-    //
-    // .[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    //   if (email.match(emailPat) == null) {         return false;     } return
-    // true; }
-    handleSubmit = (e) => {
-        const person = this.state.fields;
-        e.preventDefault();
-        if (this.validate())return;
-            this.setState(() => {
-                return {
-                    fields: {
-                        username: '',
-                        password: '',
-                        tenantId: '',
-                        url: "",
-                        driverClassName: "org.postgresql.Driver"
-                    }
-                }
-            })
-        var data = JSON.stringify(this.state.fields);
-        alert(data);
-      
-        // this.props.onSubmitTenant(data);
-    };
-    render() {
-        const divStyle = {
-            backgroundColor: 'white',
-            overflowY: 'auto',
-            width: '100%',
-            height: '600px',
-            position: 'relative'
-        };
-        return (
-            <div style={divStyle}>
-                <Nav/>
-                <hr/>
-                <div>
-                    <h6>To create new tenant, please enter following details and click Submit.</h6>
-                </div>
-                <hr/>
-                <form onSubmit={this.handleSubmit}>
-                    <Field
-                        placeholder='Datasource username'
-                        name='username'
-                        value={this.state.fields.username}
-                        validate={val => (val
-                        ? false
-                        : 'username required')}
-                        onChange={this.handleChange}/>
 
-                    <Field
-                        placeholder='Datasource password'
-                        name='password'
-                        value={this.state.fields.password}
-                        validate={val => (val
-                        ? false
-                        : 'password required')}
-                        onChange={this.handleChange}/>
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+import Nav from "../../components/Screening/MainAdminNavBar.jsx";
+import { SubmissionError } from 'redux-form';
 
-                    <Field
-                        placeholder='tenant'
-                        name='tenantId'
-                        value={this.state.fields.tenantId}
-                        validate={val => (val
-                        ? false
-                        : 'tenant required')}
-                        onChange={this.handleChange}/>
 
-                    <Field
-                        placeholder='url'
-                        name='url'
-                        value={this.state.fields.url}
-                        validate={val => (val
-                        ? false
-                        : 'url required')}
-                        onChange={this.handleChange}/>
+const required = value => (
+  value || typeof value === 'number' ? undefined : 'Required'
+);
+const maxLength = max => value =>
+  value && value.length > max ? 'Must be ' + max + ' characters or less' : undefined;
+const maxLength15 = maxLength(15);
+const maxLength30 = maxLength(30);
+export const minLength = min => value =>
+  value && value.length < min ? 'must be ' + min + ' characters or more' : undefined;
+const minLength10 = minLength(10);
+const url = value => {
+  const urlPat = new RegExp(/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/);
+  return value && !urlPat.test(value) ? 'Invalid url' : undefined
+}
+const passwordValid = value => {
+  const passwordPat = new RegExp(/^[A-Z]{1,}[a-z!@#$%^&*]*[0-9]{1,}$/);
+  return value && !passwordPat.test(value) ? 'Invalid password' : undefined
+}
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+{touched && error && <span style={{color:'red'}}>{error}</span>
+// ||(warning &&<span>{warning}</span>))
+      }
+    </div>
+  </div>
+)
 
-                    <input type="submit" disabled={this.validate()}/>
-                </form>
-            </div>
+const CreateNewTenant = props => {
+  const { error, handleSubmit, pristine, reset, submitting } = props
+  return (
+    <div>
+      <Nav />
+      <form onSubmit={handleSubmit(submit)}>
+        <Field
+          name="username"
+          type="text"
+          component={renderField}
+          label="Username"
+          validate={[required, maxLength30, minLength10]}
+          // warn={}
 
-        );
-    }
+        />
+        <Field
+          name="password"
+          type="text"
+          component={renderField}
+          label="Password"
+          validate={[required, passwordValid]}
+        />
+        <Field
+          name="tenantId"
+          type="text"
+          component={renderField}
+          label="TenantId"
+          validate={required}
+        />
+        <Field
+          name="url"
+          type="text"
+          component={renderField}
+          label="URL"
+          validate={[required,url]}
+          // warn={}
+        />
 
+        {error && <strong>{error}</strong>}
+        <div>
+          <button type="submit" disabled={submitting}>
+            Submit
+        </button>
+          <button type="button" disabled={pristine || submitting} onClick={reset}>
+            Clear Values
+        </button>
+        </div>
+      </form>
+
+    </div>
+  )
+}
+// function mapStateToProps(state){
+//     return {
+
+//     }
+// }
+
+export default reduxForm({
+  form: 'submitValidation' // a unique identifier for this form
+})(CreateNewTenant)
+
+
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+function submit(values) {
+  return sleep(1).then(() => {
+    // simulate server latency
+    // if (!['john', 'paul', 'george', 'ringo'].includes(values.username)) {
+    //   throw new SubmissionError({
+    //     username: 'User does not exist',
+    //     _error: 'Login failed!'
+    //   })
+    // } else if (values.password !== 'redux-form') {
+    //   throw new SubmissionError({
+    //     password: 'Wrong password',
+    //     _error: 'Login failed!'
+    //   })
+    // } else 
+    let driverName = "org.postgresql.Driver";
+    // data=values.concat(driverName);
+    console.log(values);
+    //let data=JSON.parse(values);
+    // console.log(data);
+    values.driverName = "org.postgresql.Driver";
+
+    window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+
+  })
 }
 
-CreateNewTenant.propTypes = {
-    added: PropTypes.bool,
-    onSubmitTenant: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({tenant: state.tenant});
-
-const mapDispatchToProps = dispatch => ({
-    onSubmitTenant: values => dispatch(UserActionCreators.createTenant(values)),
-    onClose: () => dispatch(UserActionCreators.close())
-});
-export default connect(mapStateToProps, mapDispatchToProps)(CreateNewTenant);
-
-class Field extends React.Component {
-    state = {
-        value: this.props.value,
-        error: false
-    }
-
-    static propTypes = {
-        placeholder: PropTypes.string,
-        value: PropTypes.string,
-        name: PropTypes.string.isRequired,
-        validate: PropTypes.func,
-        onChange: PropTypes.func.isRequired
-    }
-    static getDerivedStateFromProps(nextProps) {
-        return {value: nextProps.value}
-    }
-
-    onChange = (e) => {
-        const name = this.props.name;
-        const value = e.target.value;
-        const error = this.props.validate
-            ? this
-                .props
-                .validate(value)
-            : false;
-        this.setState({value, error});
-        this
-            .props
-            .onChange(name, value, error);
-    }
-    render() {
-        return (
-            <div>
-                <input
-                    placeholder={this.props.placeholder}
-                    value={this.state.value}
-                    onChange={this.onChange}></input>
-                <span style={{
-                    color: 'red'
-                }}>{this.state.error}</span>
-            </div>
-        );
-    }
-}
